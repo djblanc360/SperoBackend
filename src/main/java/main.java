d7ec0b71a95a280cc7d7ac3efa459642195211cd
Alan Manalang <a_rabbit93@yahoo.com>
2017-06-02 14:56:36 -0700

    Finally got dependency to work!

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -9,5 +9,6 @@ public class main {
         get("/hello", (req, res) -> "You always doing the most jaylen");
         get("/test", (req, res) -> "make sure you 'import' the project. NOT open." +
             " and then change the module name under project settings.");
+        get("/dependencies", (request, response) -> "Finally got the Maven dependencies to work!");
     }
 }
