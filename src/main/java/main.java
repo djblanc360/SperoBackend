a2fba9b9841443f3b937efa8943dabf94c4621a6
nguym077 <itsmariann@gmail.com>
2017-06-02 14:08:29 -0700

    last test. sorry

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -7,6 +7,7 @@ import static spark.Spark.*;
 public class main {
     public static void main(String[] args) {
         get("/hello", (req, res) -> "You always doing the most jaylen");
-        get("/test", (req, res) -> "make sure you 'import' the project. NOT open.");
+        get("/test", (req, res) -> "make sure you 'import' the project. NOT open." +
+            " and then change the module name under project settings.");
     }
 }
